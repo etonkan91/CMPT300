122fabd7b212dc0fe7928e5765285af4211d71f3
etonkan91 <etonk@sfu.ca>
2017-09-15 15:28:53 -0700

    Added support for  command, with or without arguments, whose output is redirected to a file

--- Assignment1_Shell/myShell.c
+++ Assignment1_Shell/myShell.c
@@ -10,7 +10,7 @@ int check_ampersand(char *argv[], int maxSize)
 {
 	int i = 0;
 	//printf("i am in check_ampersand \n");
-	while(argv[i] != NULL && i < maxSize)
+	while(argv[i] != NULL && i < maxSize - 1)
 	{
 		if(strcmp(argv[i],"&") == 0 && argv[i+1] == NULL)
 		{
@@ -22,17 +22,42 @@ int check_ampersand(char *argv[], int maxSize)
 	return 0;
 }
 
+int check_greater_than(char *argv[], int maxSize)
+{
+	int i = 0;
+	while(argv[i] != NULL && i < maxSize - 1)
+	{
+		if(strcmp(argv[i],">") == 0 && argv[i+1] != NULL)
+		{
+			return i+1;
+		}
+		else if (strcmp(argv[i],">") == 0 && argv[i+1] == NULL)
+		{
+			return -2;
+		}
+		else
+		{
+		i++;
+		}
+	}
+	return -1;
+}
+
+
 int main() {
-  int i;
-  int temp;
+  int i, exist_ampersand, closefd, exist_greater_than;
   char user_commands[50];
   char *argv[50]; 
   char cwd[50];
+  char filename;
   pid_t pid;
-
+  FILE *fd;
+  
   while(1) 
   {
     i = 0;
+	closefd = 0;
+	exist_ampersand = 0;
 	sigset(SIGCHLD, SIG_DFL);
 	//Ask user to type in commands
     printf("Commands:");
@@ -57,39 +82,16 @@ int main() {
     {
       // printf("I am in the != NULL ");
 	  // printf("%s ", argv[0]);
+	  if(check_ampersand(argv, 50) == 1)
+	  {
+		  exist_ampersand = 1;
+	  }
 	  //1.The internal shell command "exit" which terminates the shell
 	  if(strcmp(argv[0],"exit") == 0)
       {
         printf("GoodBye\n");
         exit(0);
       } 
-	  else if(check_ampersand(argv, 50) == 1)
-	  {
-		  //4.A command, with or without arguments, executed in the background using &
-		  //printf("I found the ampersand but will do nothing for now \n");
-		   pid = fork();
-		  //Unable to fork due to error
-		  if(pid < 0)
-		  {
-			printf("ERROR: Unable to fork\n");
-			break;
-		  }
-		  //Parent Process
-		  else if(pid != 0)
-		  {
-			sigset(SIGCHLD, wait(NULL));
-		  }
-		  //Child Process
-		  else
-		  {
-			//printf("I am before execvp and argv[0] is %s \n", argv[0]);
-			//It will read the user_command and execute the argv[0]
-			if(execvp(argv[0], argv) == -1)
-			{
-				printf("ERROR: %s \n", strerror(errno));
-			}
-		  }
-	  }
 	  else
 	  {
 		  //2. A command with no arguments
@@ -105,21 +107,50 @@ int main() {
 		  //Parent Process
 		  else if(pid != 0)
 		  {
-			wait(NULL);
+			if(exist_ampersand == 1)
+			{
+				//4.A command, with or without arguments, executed in the background using &
+				sigset(SIGCHLD, wait(NULL));
+			}
+			else
+			{
+				wait(NULL);
+			}
 		  }
 		  //Child Process
 		  else
 		  {
-			//printf("I am before execvp and argv[0] is %s \n", argv[0]);
-			//It will read the user_command and execute the argv[0]
-			if(execvp(argv[0], argv) == -1)
+			exist_greater_than = check_greater_than(argv, 50);
+			if(exist_greater_than > 0)
 			{
-				printf("ERROR: %s \n", strerror(errno));
+				fd = freopen(argv[exist_greater_than], "w", stdout);
+				argv[exist_greater_than - 1] = NULL;
+				argv[exist_greater_than] = NULL;
+				closefd = 1;
+				if(execvp(argv[0], argv) == -1)
+				{
+					printf("ERROR: %s \n", strerror(errno));
+				}
+			}
+			else if(exist_greater_than == -2)
+			{
+				printf("Error: missing file name in the command");
+			}
+			//It will read the user_command and execute the argv[0]
+			else
+			{
+				if(execvp(argv[0], argv) == -1)
+				{
+					printf("ERROR: %s \n", strerror(errno));
+				}
+			}
+			if(closefd == 1)
+			{
+				fclose(fd);
 			}
 		  }
 	  }
     }
   }
-
   return 0;
 }
