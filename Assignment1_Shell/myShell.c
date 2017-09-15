dde7a8a78ab04caedab0049b5318e0bcf86e35bd
etonkan91 <etonk@sfu.ca>
2017-09-15 15:26:03 -0700

    Added support for command, with or without arguments, executed in the background using &

--- Assignment1_Shell/myShell.c
+++ Assignment1_Shell/myShell.c
@@ -4,20 +4,36 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <signal.h>
 
+int check_ampersand(char *argv[], int maxSize)
+{
+	int i = 0;
+	//printf("i am in check_ampersand \n");
+	while(argv[i] != NULL && i < maxSize)
+	{
+		if(strcmp(argv[i],"&") == 0 && argv[i+1] == NULL)
+		{
+			argv[i] = NULL;
+			return 1;
+		}
+		i++;
+	}
+	return 0;
+}
 
 int main() {
   int i;
   int temp;
   char user_commands[50];
   char *argv[50]; 
-  char *tokened[10];
   char cwd[50];
   pid_t pid;
 
   while(1) 
   {
     i = 0;
+	sigset(SIGCHLD, SIG_DFL);
 	//Ask user to type in commands
     printf("Commands:");
     //Get the user commands
@@ -37,7 +53,6 @@ int main() {
 		argv[++i] = strtok(NULL, " \t\n");
 	}
 	
-	
     if(argv[0] != NULL)
     {
       // printf("I am in the != NULL ");
@@ -48,31 +63,60 @@ int main() {
         printf("GoodBye\n");
         exit(0);
       } 
-	  
-	  //2. A command with no arguments
-	  //3. A command with arguments
-	  pid = fork();
-	  //printf("pid is %d \n",pid);
-	  //Unable to fork due to error
-	  if(pid < 0)
+	  else if(check_ampersand(argv, 50) == 1)
 	  {
-		printf("ERROR: Unable to fork\n");
-		break;
+		  //4.A command, with or without arguments, executed in the background using &
+		  //printf("I found the ampersand but will do nothing for now \n");
+		   pid = fork();
+		  //Unable to fork due to error
+		  if(pid < 0)
+		  {
+			printf("ERROR: Unable to fork\n");
+			break;
+		  }
+		  //Parent Process
+		  else if(pid != 0)
+		  {
+			sigset(SIGCHLD, wait(NULL));
+		  }
+		  //Child Process
+		  else
+		  {
+			//printf("I am before execvp and argv[0] is %s \n", argv[0]);
+			//It will read the user_command and execute the argv[0]
+			if(execvp(argv[0], argv) == -1)
+			{
+				printf("ERROR: %s \n", strerror(errno));
+			}
+		  }
 	  }
-	  //Parent Process
-	  else if(pid != 0)
-	  {
-		wait(NULL);
-	  }
-	  //Child Process
 	  else
 	  {
-		//printf("I am before execvp and argv[0] is %s \n", argv[0]);
-		//It will read the user_command and execute the argv[0]
-		if(execvp(argv[0], argv) == -1)
-		{
-			printf("ERROR: %s \n", strerror(errno));
-		}
+		  //2. A command with no arguments
+		  //3. A command with arguments
+		  pid = fork();
+		  //printf("pid is %d \n",pid);
+		  //Unable to fork due to error
+		  if(pid < 0)
+		  {
+			printf("ERROR: Unable to fork\n");
+			break;
+		  }
+		  //Parent Process
+		  else if(pid != 0)
+		  {
+			wait(NULL);
+		  }
+		  //Child Process
+		  else
+		  {
+			//printf("I am before execvp and argv[0] is %s \n", argv[0]);
+			//It will read the user_command and execute the argv[0]
+			if(execvp(argv[0], argv) == -1)
+			{
+				printf("ERROR: %s \n", strerror(errno));
+			}
+		  }
 	  }
     }
   }
